edd438baea42e919cb61c3bdcb8c208debd5ba5b
c0der97 <blae1997@gmail.com>
2021-05-26 21:34:13 -0500

    Version inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-landing',
+  templateUrl: './landing/landing.component.html',
+  styleUrls: ['./landing/landing.component.scss']
 })
 export class AppComponent {
   title = 'Wolox';
