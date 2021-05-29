5047cb7c94c547f181881e2048769d6e5596cd40
c0der97 <blae1997@gmail.com>
2021-05-29 18:00:45 -0500

    Avances Landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
